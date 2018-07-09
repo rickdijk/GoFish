715577cfa5e91351f3a29317756901caf693ca17
Rick van Dijk <rickavdijk@gmail.com>
2018-07-09 13:09:36 +0200

    First set-up for fishing game

---
+++ GoFish/GoFish/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GoFish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textProgress = new System.Windows.Forms.TextBox();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listHand = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonAsk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your name";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(16, 30);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 20);
+            this.textName.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(122, 28);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(188, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start the game!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Game Progress";
+            // 
+            // textProgress
+            // 
+            this.textProgress.Location = new System.Drawing.Point(16, 70);
+            this.textProgress.Multiline = true;
+            this.textProgress.Name = "textProgress";
+            this.textProgress.ReadOnly = true;
+            this.textProgress.Size = new System.Drawing.Size(294, 283);
+            this.textProgress.TabIndex = 4;
+            // 
+            // textBooks
+            // 
+            this.textBooks.Location = new System.Drawing.Point(16, 377);
+            this.textBooks.Multiline = true;
+            this.textBooks.Name = "textBooks";
+            this.textBooks.ReadOnly = true;
+            this.textBooks.Size = new System.Drawing.Size(294, 205);
+            this.textBooks.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Books";
+            // 
+            // listHand
+            // 
+            this.listHand.FormattingEnabled = true;
+            this.listHand.Location = new System.Drawing.Point(386, 28);
+            this.listHand.Name = "listHand";
+            this.listHand.Size = new System.Drawing.Size(210, 498);
+            this.listHand.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Your hand";
+            // 
+            // buttonAsk
+            // 
+            this.buttonAsk.Enabled = false;
+            this.buttonAsk.Location = new System.Drawing.Point(386, 559);
+            this.buttonAsk.Name = "buttonAsk";
+            this.buttonAsk.Size = new System.Drawing.Size(210, 23);
+            this.buttonAsk.TabIndex = 9;
+            this.buttonAsk.Text = "Ask for a card";
+            this.buttonAsk.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 605);
+            this.Controls.Add(this.buttonAsk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listHand);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textProgress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textProgress;
+        private System.Windows.Forms.TextBox textBooks;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listHand;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonAsk;
+    }
+}
+
---
+++ GoFish/GoFish/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFish
+{
+    class Game
+    {
+        private string text;
+        private List<string> list;
+        private TextBox textProgress;
+
+        public Game(string text, List<string> list, TextBox textProgress)
+        {
+            this.text = text;
+            this.list = list;
+            this.textProgress = textProgress;
+        }
+
+        public IEnumerable<string> GetPlayerCardNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DescribeBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DescribePlayerHands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayOneRound(int selectedIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetWinnerName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
